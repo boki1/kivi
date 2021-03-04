3d745746b3ed410c55c8826bf73092287f2081c4
boki1 <kristoimenov@gmail.com>
2021-03-04 21:48:45 +0200

    Add new directory for generated files

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,29 @@
 #include <fstream>
 #include <iostream>
 
-#include "include/kivi_parser.tab.hh"
+#include "autogen/kivi_parser.tab.hh"
 #include "include/parser.hpp"
 
-namespace yy {
-    yy::kivi_parser::symbol_type yylex(parsing_context &ctx);
+static void
+usage ()
+{
+    std::cout << "Use `kivi main.kv`" << '\n';
 }
 
-static void usage() { std::cout << "Use `kivi main.kv`" << '\n'; }
+int
+main (int argc, char *const argv[])
+{
+    if (argc != 2)
+	{
+	    usage ();
+	    return 0;
+	}
 
+    const std::string filename = argv[1];
+    std::ifstream ifs (filename);
+    std::string code (std::istreambuf_iterator<char> (ifs), {});
 
-int main(int argc, char *const argv[]) {
-  if (argc != 2) {
-    usage();
+    parsing_context ctx (code.c_str (), &filename);
+
     return 0;
-  }
-
-  std::string filename = argv[1];
-  std::ifstream ifs(filename);
-  std::string code(std::istreambuf_iterator<char>(ifs), {});
-
-  parsing_context ctx(code.c_str(), &filename);
-
-  return 0;
 }
