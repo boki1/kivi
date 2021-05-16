a2e156ea88be364be5107546cab9f4a7d3c190d3
vili <vkabadzhova@gmail.com>
2021-05-16 18:20:29 +0300

    Enhance TAC structure

--- frontend/ir_generation/ir_code.cc
+++ frontend/ir_generation/ir_code.cc
@@ -1,16 +1,32 @@
-#include <variant>
-#include <vector>
 #include "ir_code.hh"
 
 #include <parser/parser.tab.hh>
 
 namespace intermediate_representation {
+    tac::tac(tac::type &tac_type, std::vector<fake_register_type> &operands) : m_type(tac_type),
+                                                                               m_operands(std::move(operands)) {}
+
+    tac::tac(tac::type &tac_type, std::string_view str /*  = nullptr */, int i /*  = 0 */) : m_type(tac_type){
+
+    }
+
+    tac::tac(std::shared_ptr<tac> b, std::vector<fake_register_type> &operands,
+             tac::type tac_type /* = type::IfNotZero */) :
+            m_condition(b), m_operands(std::move(operands)), m_type(tac_type) {}
+
+
+    tac::tac(std::vector<fake_register_type> &operands) : m_operands(std::move(operands)) {}
+
     tac::type tac::tac_type() const {
         return m_type;
     }
 
-    const tac::atom_type &tac::atom() const {
-        return m_atom;
+    std::string tac::identifier() const {
+        return m_identifier;
+    }
+
+    off_t tac::value() const {
+        return m_value;
     }
 
     const std::shared_ptr<tac> &tac::next() const {
@@ -21,9 +37,10 @@ namespace intermediate_representation {
         return m_condition;
     }
 
-    const std::vector <tac::fake_register_type> &tac::operands() const {
+    const std::vector<tac::fake_register_type> &tac::operands() const {
         return m_operands;
     }
+
 };
 
 
--- frontend/ir_generation/ir_code.hh
+++ frontend/ir_generation/ir_code.hh
@@ -6,12 +6,11 @@
 #include <vector>
 #include <string>
 #include <variant>
+#include <string_view>
 
 namespace intermediate_representation {
     class tac {
     public:
-        /// The type of the atom when one is required
-        typedef std::variant<std::monostate, int, std::string> atom_type;
         /// The indexes of the fake registers
         typedef int fake_register_type;
         /**
@@ -19,7 +18,7 @@ namespace intermediate_representation {
          */
         enum class type {
             Nop,            //< No operation
-            Init,           //< Assign value
+            Init,           //< Store a pointer to a value at address &identifier + offset where identifier is a function's base
             Add,            //< Perform addition
             Negate,         //< Negation
             Copy,           //< Assign a copy of another variable
@@ -35,8 +34,12 @@ namespace intermediate_representation {
         /// The type of the tac
         tac::type m_type{type::Nop};
 
-        /// The atom
-        tac::atom_type m_atom;
+        /// Reference to functions name (label)
+        /// @note empty = none
+        std::string m_identifier{};
+
+        /// Offset
+        off_t m_value{};
 
         /// Pointer to next statement in the chain. The end is marked with nullptr
         std::shared_ptr<tac> m_next{nullptr};
@@ -49,19 +52,19 @@ namespace intermediate_representation {
 
     public:
         tac() = default;
+        tac(tac::type &tac_type, std::vector<fake_register_type> &operands);
+        tac(tac::type &tac_type, std::string_view str = nullptr, int i = 0);
+        tac(std::shared_ptr<tac> b, std::vector<fake_register_type> &operands, tac::type tac_type = type::IfNotZero);
+        tac(std::vector<fake_register_type> &operands);
         ~tac() = default;
 
     public:
         [[nodiscard]] type tac_type() const;
-
-        [[nodiscard]] const atom_type &atom() const;
-
-        [[nodiscard]] const std::shared_ptr <tac> &next() const;
-
+        [[nodiscard]] std::string identifier() const;
+        [[nodiscard]] off_t value() const;
+        [[nodiscard]] const std::shared_ptr<tac> &next() const;
         [[nodiscard]] const std::optional<std::shared_ptr<tac>> &condition() const;
-
         [[nodiscard]] const std::vector<fake_register_type> &operands() const;
-
     };
 }
 
