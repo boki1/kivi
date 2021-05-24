13149b010b44217e4d4f5cb4c682b1ec1fc7e5dd
boki1 <kristoimenov@gmail.com>
2021-05-24 19:14:55 +0300

    Fix building system + travis support

--- test/frontend/expression.test.cc
+++ test/frontend/expression.test.cc
@@ -12,38 +12,37 @@ using std::move;
 using namespace syntax_analyzer;
 using et = expression::type;
 
-TEST_CASE("Expression class", "[expression]"){
-	SECTION("operator== : regular"){expression expr1{et::Addition, {expression(1), expression(2)}};
-expression expr2{expr1};
-
-REQUIRE(expr1 == expr2);
-}
-
-SECTION("operator== : regular peculiar")
+TEST_CASE("Expression class", "[expression]")
 {
-	// expr1 <=> if 1: return 1;
-	expression expr1 = expression{expression::type::If,
-								  make_shared<expression>(move(expression(expression(1)))),
-								  {move(expression(et::Return, {expression(1)}))}};
+	SECTION("operator== : regular")
+	{
+		expression expr1{et::Addition, {expression(1), expression(2)}};
+		expression expr2{expr1};
 
-	expression expr2{expr1};
+		REQUIRE(expr1 == expr2);
+	}
 
-	REQUIRE(expr1 == expr2);
+	SECTION("operator== : regular peculiar")
+	{
+		// expr1 <=> if 1: return 1;
+		expression expr1 = expression{expression::type::If,
+									  {move(expression(expression(1))), move(expression(et::Return, {expression(1)}))}};
+
+		expression expr2{expr1};
+
+		REQUIRE(expr1 == expr2);
+	}
+
+	SECTION("operator== : negative peculiar")
+	{
+		// expr1 <=> if 1: return 1;
+		expression expr1 = expression{expression::type::If,
+									  {move(expression(expression(1))), move(expression(et::Return, {expression(1)}))}};
+
+		// expr2 <=> if 1: return 3;
+		expression expr2 = expression{expression::type::If,
+									  {move(expression(expression(1))), move(expression(et::Return, {expression(3)}))}};
+
+		REQUIRE(expr1 != expr2);
+	}
 }
-
-SECTION("operator== : negative peculiar")
-{
-	// expr1 <=> if 1: return 1;
-	expression expr1 = expression{expression::type::If,
-								  make_shared<expression>(move(expression(expression(1)))),
-								  {move(expression(et::Return, {expression(1)}))}};
-
-	// expr2 <=> if 1: return 3;
-	expression expr2 = expression{expression::type::If,
-								  make_shared<expression>(move(expression(expression(1)))),
-								  {move(expression(et::Return, {expression(5)}))}};
-
-	REQUIRE(expr1 != expr2);
-}
-}
-;
