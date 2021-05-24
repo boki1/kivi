32741c2cec9af3ac43b3c4dab14b866c05e1dd3f
boki1 <kristoimenov@gmail.com>
2021-05-24 21:19:48 +0300

    Move code to src/

---
+++ src/README.md
@@ -0,0 +1,13 @@
+## Src
+
+This directory contains the whole source code of the Kivi compiler.
+
+--------------
+
+**Contents**
+
+> `frontend/`
+In this folder is contained the source code of the compiler frontend.
+
+> `backend/`
+You probably guessed it :smile:.
--- tools/clean.sh
+++ tools/clean.sh
@@ -2,7 +2,10 @@ rm -rf build
 rm -rf docs/build
 rm -rf Testing
 rm -rf Debug
-rm -r frontend/parser/*tab*
-rm -r frontend/parser/position.hh
-rm -r frontend/parser/location.hh
-rm -r frontend/parser/stack.hh
+
+PARSER_DIR=src/frontend/parser
+
+rm -f $PARSER_DIR/*tab*
+rm -f $PARSER_DIR/position.hh
+rm -f $PARSER_DIR/location.hh
+rm -f $PARSER_DIR/stack.hh
