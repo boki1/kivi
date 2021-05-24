5d7f26a17883994f4a4566bc769b00c5f24aa0dd
boki1 <kristoimenov@gmail.com>
2021-05-24 19:30:30 +0300

    Fix travis build + unit test

--- test/frontend/parser.test.cc
+++ test/frontend/parser.test.cc
@@ -3,6 +3,7 @@
  * @brief Tests the syntactical analysis stage of the compiler
  */
 
+#include <bits/types/struct_sched_param.h>
 #include <memory>
 #include <vector>
 
@@ -10,8 +11,8 @@
 #include <parser/parser.tab.hh>
 #include <parser/parsing_context.hh>
 
-using std::move;
 using std::make_shared;
+using std::move;
 
 using namespace syntax_analyzer;
 using et = expression::type;
@@ -24,29 +25,26 @@ TEST_CASE("Syntactical analysis", "[parser]")
 	{
 		std::string code = "foo n: return 1;";
 		parsing_context ctx(code.c_str(), &fake_file);
-		yy::kivi_parser parser{ ctx };
+		yy::kivi_parser parser{ctx};
 
 		REQUIRE(parser.parse() == 0);
 
-		function expected{
-			"foo",
-			expression{
-				et::Return,
-				{ expression(1) }
-			},
-			0,
-			1
-		};
+		auto expected = sa::expression {et::Return, {expression(1)}};
+		auto implicit = sa::expression {et::Return, {expression(0)}};
+		const auto &fun = ctx.functions().at(0);
 
 		REQUIRE(ctx.functions().size() == 1);
-		REQUIRE(expected == ctx.functions().at(0));
+		REQUIRE(expected == fun.body().operands().at(0));
+		REQUIRE(implicit == fun.body().operands().at(1));
+		REQUIRE(fun.parameters() == 1);
+		REQUIRE(fun.locals() == 0);
 	}
 
 	SECTION("Simple function parse : syntax error")
 	{
 		std::string code = "foo: { auto a = 3; auto a = 4; } ";
 		parsing_context ctx(code.c_str(), &fake_file);
-		yy::kivi_parser parser{ ctx };
+		yy::kivi_parser parser{ctx};
 
 		REQUIRE(parser.parse() == 1);
 	}
